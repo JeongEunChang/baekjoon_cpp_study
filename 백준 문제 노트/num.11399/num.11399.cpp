59dce3a97c3c692a8b40a864795937367e47e41f
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-07-13 19:58:19 +0900

    2022.07.13
    + 11399(ATM) -> 성공

---
+++ 백준 문제 노트/num.11399/num.11399.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int PersonNum;
+    int Result = 0;
+    int Time = 0;
+    vector<int> PersonVec;
+
+    cin >> PersonNum;
+
+    for (int i = 0; i < PersonNum; i++)
+    {
+        int Input;
+
+        cin >> Input;
+
+        PersonVec.push_back(Input);
+    }
+
+    sort(PersonVec.begin(), PersonVec.end());
+
+    for (vector<int>::size_type i = 0; i < PersonVec.size(); i++)
+    {
+        Time += PersonVec[i];
+        Result += Time;
+    }
+
+    cout << Result;
+}
