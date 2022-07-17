dce43d8b85284b29372137d31e9d8e4aca5a79f1
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-07-17 18:20:46 +0900

    2022.07.17
    + 2839(설탕 배달) -> 성공
    + 11723(집합) -> 성공

---
+++ 백준 문제 노트/num.11723/num.11723.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#pragma warning (disable:4996)
+using namespace std;
+
+int main()
+{
+    int TestCase;
+    int Arr[21] = {0};
+    char Input[7];
+    int Value = 0;
+
+    scanf("%d", &TestCase);
+
+    for (int i = 0; i < TestCase; i++)
+    {
+        scanf("%s", Input);
+
+        if (!strcmp(Input, "add"))
+        {
+            scanf("%d", &Value);
+
+            if (Arr[Value] == 0)
+                Arr[Value] = Value;
+        }
+
+        else if (!strcmp(Input, "remove"))
+        {
+            scanf("%d", &Value);
+
+            if (Arr[Value] != 0)
+                Arr[Value] = 0;
+        }
+
+        else if (!strcmp(Input, "check"))
+        {
+            scanf("%d", &Value);
+
+            if (Arr[Value] == 0)
+                printf("%d\n", 0);
+
+            else
+                printf("%d\n", 1);
+        }
+
+        else if (!strcmp(Input, "toggle"))
+        {
+            scanf("%d", &Value);
+
+            if (Arr[Value] == 0)
+                Arr[Value] = Value;
+
+            else
+                Arr[Value] = 0;
+        }
+
+        else if (!strcmp(Input, "all"))
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Arr[i] = i;
+            }
+
+        }
+
+        else if (!strcmp(Input, "empty"))
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Arr[i] = 0;
+            }
+        }
+    }
+}
---
+++ 백준 문제 노트/num.2839/num.2839.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdio.h>
+#pragma warning (disable:4996)
+using namespace std;
+
+int main()
+{
+    int N = 0;
+
+    scanf("%d", &N);
+    int tmp = (5 - (N % 5)) + N;
+
+    if (N % 5 == 0)
+    {
+        printf("%d", N / 5);
+        return 0;
+    }
+
+    else
+    {
+        while (tmp > 0)
+        {
+            tmp -= 5;
+
+            if ((N - tmp) % 3 == 0)
+            {
+                printf("%d", (tmp / 5) + ((N - tmp) / 3));
+                return 0;
+            }
+        }
+    }
+
+    printf("%d", -1);
+}
