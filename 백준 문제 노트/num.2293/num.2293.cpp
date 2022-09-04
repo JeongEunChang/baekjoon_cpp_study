11bfe1ea260cfd86317402b191354546df07f7ae
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-04 14:53:16 +0900

    2022.09.04
    + 2293(동전 1) -> 성공

---
+++ 백준 문제 노트/num.2293/num.2293.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+int Array[101];
+int DP[10001];
+
+int main()
+{
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Array[i];
+    }
+
+    sort(Array, Array + N);
+
+    DP[0] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = Array[i]; j <= K; j++)
+        {
+            DP[j] = DP[j] + DP[j - Array[i]];
+        }
+    }
+
+    cout << DP[K];
+}
