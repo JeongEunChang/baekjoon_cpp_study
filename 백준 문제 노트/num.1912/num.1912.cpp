3c94d1aac9f4a44f79122efcfa4be63fdb45462f
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-06 19:36:27 +0900

    2022.09.06
    + 1912(연속합) -> 성공

---
+++ 백준 문제 노트/num.1912/num.1912.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int N;
+int Answer = INT_MIN;
+int Array[100001];
+int DP[100001];
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Array[i];
+    }
+
+    DP[1] = Array[1];
+
+    for (int i = 2; i <= N; i++)
+    {
+        DP[i] = max(DP[i - 1] + Array[i], Array[i]);
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        Answer = max(DP[i], Answer);
+    }
+
+    cout << Answer;
+}
