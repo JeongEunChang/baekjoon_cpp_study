b96aa1b6a2446214d45f5f60beec2f656c115c91
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-08 12:25:27 +0900

    2022.09.08
    + 9095(1, 2, 3 더하기) -> 성공

---
+++ 백준 문제 노트/num.9095/num.9095.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int DP[12];
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int Input;
+
+        cin >> Input;
+
+        DP[1] = 1;
+        DP[2] = 2;
+        DP[3] = 4;
+
+        for (int i = 4; i <= Input; i++)
+        {
+            DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
+        }
+
+        cout << DP[Input] << endl;
+    }
+}
