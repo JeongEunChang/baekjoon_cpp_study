1f22f1b2b284be64c89248d94c250dcfd5609199
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-09 14:47:14 +0900

    2022.09.09
    + 11052(카드 구매하기) -> 성공

---
+++ 백준 문제 노트/num.11052/num.11052.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int Array[1001];
+int DP[1001];
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Array[i];
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            DP[i] = max(DP[i], DP[i - j] + Array[j]);
+        }
+    }
+
+    cout << DP[N];
+}
