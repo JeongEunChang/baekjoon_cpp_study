f961af108afed6b922bba7703ad73c3b6cfff5dc
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-11 02:50:18 +0900

    2022.09.11
    + 2294(동전 2) -> 성공

--- 백준 문제 노트/asdfasf/asdfasf.cpp
+++ 백준 문제 노트/asdfasf/asdfasf.cpp
@@ -1,38 +1,81 @@
 ﻿#include <iostream>
+#include <cstring>
 using namespace std;
 
-int n;
-const int maxi = 100001;
-const int mod = 9901;
-int zoo[maxi][3];//사자를 가두는 경우의 수
+int Arr[10] = { 5, 5, 10, 1, 0, 8, 6, 7, 2, 3 };
 
-//입력
-void input() {
-	cin >> n;
-}
+void Merge(int Left, int Mid, int Right)
+{
+	int tmpArr[10] = { 0 };
+	int L = Left;
+	int R = Right;
+	int tmpPointer = Left;
+	int M = Mid + 1;
 
-//사자를 가두는 경우의 수
-void Lion() {
-	//초기값
-	zoo[1][0] = 1;//선택x
-	zoo[1][1] = 1;//왼쪽 선택
-	zoo[1][2] = 1;//오른쪽 선택
+	while (L <= Mid && M <= Right)
+	{
+		if (Arr[L] <= Arr[M])
+		{
+			tmpArr[tmpPointer++] = Arr[L++];
+		}
 
-	//점화식
-	for (int i = 2; i <= n; i++) {
-		zoo[i][0] = (zoo[i - 1][0] + zoo[i - 1][1] + zoo[i - 1][2]) % mod;//선택X
-		zoo[i][1] = (zoo[i - 1][0] + zoo[i - 1][2]) % mod;//왼쪽 선택
-		zoo[i][2] = (zoo[i - 1][0] + zoo[i - 1][1]) % mod;//오른쪽 선택
+		else
+		{
+			tmpArr[tmpPointer++] = Arr[M++];
+		}
 	}
-	//출력
-	cout << (zoo[n][0] + zoo[n][1] + zoo[n][2]) % mod;
-}
-int main() {
-	//빠른 입력
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
 
-	input();//입력
-	Lion();//사자를 가두는 경우의 수
-	return 0;
+	if (L > Mid)
+	{
+		while (M <= Right)
+		{
+			tmpArr[tmpPointer++] = Arr[M++];
+		}		
+	}
+
+	else
+	{
+		while (L <= Mid)
+		{
+			tmpArr[tmpPointer++] = Arr[L++];
+		}
+	}
+
+	for (int i = Left; i <= Right; i++)
+	{
+		Arr[i] = tmpArr[i];
+		cout << Arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+void Device(int Left, int Right)
+{
+	if (Left < Right)
+	{
+		int Mid = (Left + Right) / 2;
+		Device(Left, Mid);
+		Device(Mid + 1, Right);
+		Merge(Left, Mid, Right);
+	}
+}
+
+
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << Arr[i] << " ";
+	}
+
+	cout << endl;
+
+	Device(0, (sizeof(Arr) / sizeof(int)) - 1);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << Arr[i] << " ";
+	}
 }
---
+++ 백준 문제 노트/num.2294/num.2294.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+int Array[101];
+int DP[10001];
+
+int main()
+{
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Array[i];
+    }
+
+    for (int i = 1; i <= K; i++)
+    {
+        DP[i] = 10002;
+    }
+
+    sort(Array, Array + N);
+
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = Array[i]; j <= K; j++)
+        {
+            DP[j] = min(DP[j], DP[j - Array[i]] + 1);
+        }
+    }
+
+    if (DP[K] == 10002)
+        cout << -1;
+
+    else
+        cout << DP[K];
+}
