c1d1c100c1eacd27a561986cbfe83c1869cafd26
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-12 20:29:48 +0900

    2022.09.12
    + 11053(r가장 긴 증가하는 부분) -> 성공

---
+++ 백준 문제 노트/num.11053/num.11053.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int A, Answer;
+int Array[1001];
+int DP[1001];
+
+int main()
+{
+    cin >> A;
+
+    for (int i = 1; i <= A; i++)
+    {
+        cin >> Array[i];
+    }
+
+    for (int i = 1; i <= A; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (Array[j] < Array[i])
+            {
+                DP[i] = max(DP[j] + 1, DP[i]);
+            } 
+        }
+    }
+
+    for (int i = 1; i <= A; i++)
+    {
+        if (DP[i] > Answer)
+            Answer = DP[i];
+    }
+
+    cout << Answer;
+}
