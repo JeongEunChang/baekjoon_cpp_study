fd3036f70346a05064815c1fdc4713cb7f6593c6
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-15 11:34:04 +0900

    2022.09.15
    + 1890(점프) -> 성공
    + 11726(2xn 타일링) -> 성공

---
+++ 백준 문제 노트/num.11726/num.11726.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int N;
+int DP[1001];
+
+int main()
+{
+    cin >> N;
+
+    DP[0] = 1;
+    DP[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+    {
+        DP[i] = DP[i - 1] % 10007 + DP[i - 2] % 10007;
+    }
+
+    cout << DP[N] % 10007;
+}
---
+++ 백준 문제 노트/num.1890/num.1890.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+int N;
+int Array[101][101];
+int DP[101][101];
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            cin >> Array[i][j];
+        }
+    }
+
+    DP[1 + Array[1][1]][1] = 1;
+    DP[1][1 + Array[1][1]] = 1;
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            if (i == 1 && j == 1)
+                continue;
+
+            if (i == N && j == N)
+                continue;
+
+            if (DP[i][j] == 0)
+            {
+                continue;
+            }
+
+            if (i + Array[i][j] <= N)
+            {
+                DP[i + Array[i][j]][j] += DP[i][j];              
+            }
+
+            if (j + Array[i][j] <= N)
+            {
+                DP[i][j + Array[i][j]] += DP[i][j];
+            }
+        }
+    }
+
+    cout << DP[N][N];
+}
