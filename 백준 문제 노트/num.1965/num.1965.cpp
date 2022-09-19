3dbd7a0263cc7b9e49067f831d18c7da090b8000
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-09-19 18:32:20 +0900

    2022.09.19
    + 1965(상자넣기) -> 성공

---
+++ 백준 문제 노트/num.1965/num.1965.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int N, Answer;
+int Array[1001];
+int DP[1001];
+
+int main()
+{
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> Array[i];
+    }
+
+    DP[1] = 1;
+
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            if (Array[i] > Array[j])
+            {
+                DP[i] = max(DP[i], DP[j] + 1);
+            }
+        }
+
+        if (DP[i] == 0)
+            DP[i] = 1;
+    }
+    
+    for (int i = 1; i <= N; i++)
+    {
+        Answer = max(Answer, DP[i]);
+    }
+
+    cout << Answer;
+}
